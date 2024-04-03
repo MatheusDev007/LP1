3849fae44007496abe8764af3c194de5081c2838
Matheus Evangelista <matheusmotorista070@gmail.com>
2024-04-03 10:57:42 -0300

    updt1057

--- JAVA/projeto27032024/src/Main.java
+++ JAVA/projeto27032024/src/Main.java
@@ -1,15 +1,25 @@
 import java.util.Scanner;
 
-public class App {
+public class Main {
+    
     public static void main(String[] args) throws Exception {
         Scanner ler = new Scanner(System.in);
         Cliente objCliente = new Cliente();
         
         System.out.println("Digite o seu nome: ");
         objCliente.nome = ler.next();
-
-        System.out.println("Digite o seu CPF: ");
-        objCliente.cpf = ler.next();
+        
+        boolean cpfValido = false;
+        while (!cpfValido) {
+            System.out.println("Digite o seu CPF: ");
+            String cpf = ler.next();
+            if (ValidaCPF(cpf)) {
+                objCliente.cpf = cpf;
+                cpfValido = true;
+            } else {
+                System.out.println("CPF inválido. Digite novamente.");
+            }
+        }
 
         System.out.println("Digite o seu RG: ");
         objCliente.rg = ler.next();
@@ -29,4 +39,13 @@ public class App {
         ler.close();
     }
     
+    public static boolean ValidaCPF (String cpf) {
+        if(cpf.length() != 14){
+            return false;
+        }else if(cpf.charAt(3) != '.' && cpf.charAt(7) != '.' && cpf.charAt(11) != '-'){
+            return false;
+    }else{
+            return true;
+    }
+        }
 }
